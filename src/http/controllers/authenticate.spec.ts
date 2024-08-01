a3678f0ed666ed7bb44cd125be97275ebd950234
RennanPereira <rennan94pereira@gmail.com>
2024-07-31 21:44:39 -0300

    env fixed and create register spec

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,33 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => { })
+describe('Register (e2e)', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to Authenticate', async () => {
+        await request(app.server)
+            .post('/users').send({
+                name: 'John Doe',
+                email: 'johndoe@exemple.com',
+                password: '123456',
+            })
+
+        const response = await request(app.server).post('/sessions').send({
+            email: 'johndoe@exemple.com',
+            password: '123456',
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String),
+        })
+    })
+})
